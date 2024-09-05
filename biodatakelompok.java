58c5d2c7c907bae27b62fc3160ac21dea0ff8081
sharren elvaretta <sharrenepg@gmail.com>
2024-09-05 15:29:57 +0700

    biodata sharren

--- biodatakelompok.java
+++ biodatakelompok.java
@@ -5,5 +5,8 @@ public class biodatakelompok {
 
     public static void main(String[] args) {
         System.out.println("biodata team");
+        System.out.println("nama: sharren ");
+        System.out.println("NIM: 244107020191");
+        System.out.println("kelas: TI IF");
     }
 }
