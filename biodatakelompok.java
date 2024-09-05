0dd6a2fd556557880689edd7e6b91aa2d5299f7c
nabillahumi <nabillahumi003@gmail.com>
2024-09-05 15:30:28 +0700

    modified:   biodatakelompok.java

--- biodatakelompok.java
+++ biodatakelompok.java
@@ -5,5 +5,8 @@ public class biodatakelompok {
 
     public static void main(String[] args) {
         System.out.println("biodata team");
+        System.out.println("nama : Nabillah Umi Purnama");
+        System.out.println("NIM : 2441070200554");
+        System.out.println("kelas : 1F");
     }
 }
