dcb5af5f4385ea059d071f8938512510ce009333
wawaelenti <irawantielent@gmail.com>
2024-09-05 15:37:07 +0700

    modified:   biodatakelompok.java

--- biodatakelompok.java
+++ biodatakelompok.java
@@ -5,5 +5,8 @@ public class biodatakelompok {
 
     public static void main(String[] args) {
         System.out.println("biodata team");
+        System.out.println("Nama: Wawa Elent Irawanti");
+        System.out.println("NIM: 244107020192");
+        System.out.println("Kelas: 1F");
     }
 }
